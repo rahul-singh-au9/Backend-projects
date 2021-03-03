90d01540f4e0e2ae38b66a5aec8bab9af07e0a7f
unknown <rahulsg1508@gmail.com>
2021-03-03 23:33:17 +0530

    PWA

---
+++ pwa/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        this is a react app with PWA Features <br/>
+        PWA -- progressive web application.
+      </h1>
+    </div>
+  );
+}
+
+export default App;
